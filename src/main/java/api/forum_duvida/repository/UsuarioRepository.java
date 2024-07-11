df47c436cf888a173d6d1724766d96a23df95891
arturxdking <arturxdking@gmail.com>
2024-07-11 17:37:39 -0300

    Finalizando projeto

---
+++ src/main/java/api/forum_duvida/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package api.forum_duvida.repository;
+
+import api.forum_duvida.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByLogin(String login);
+}
